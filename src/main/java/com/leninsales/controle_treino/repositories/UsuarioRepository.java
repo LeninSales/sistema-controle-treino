1c8ed9f04d8041831c87ee386cb25dea7ddf5736
Lenin Sales <leninsalesoliveira@gmail.com>
2025-10-08 14:51:23 -0300

    feat(usuario): repositorio do usuario criado

--- src/main/java/com/leninsales/controle_treino/repositories/UsuarioRepository.java
+++ src/main/java/com/leninsales/controle_treino/repositories/UsuarioRepository.java
@@ -1,4 +1,9 @@
 package com.leninsales.controle_treino.repositories;
 
-public interface UsuarioRepository {
+import com.leninsales.controle_treino.models.UsuarioModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UsuarioRepository extends JpaRepository <UsuarioModel, UUID> {
 }
