eb3203b50a89a3fe466defab9d272d2260dff012
Lenin Sales <leninsalesoliveira@gmail.com>
2025-10-13 18:30:26 -0300

    feat(rotina): criar RotinaRepository estendendo JpaRepository

---
+++ src/main/java/com/leninsales/controle_treino/repositories/RotinaRepository.java
@@ -0,0 +1,7 @@
+package com.leninsales.controle_treino.repositories;
+
+import com.leninsales.controle_treino.models.RotinaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RotinaRepository extends JpaRepository<RotinaModel, Integer> {
+}
