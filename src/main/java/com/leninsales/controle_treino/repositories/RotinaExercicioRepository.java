9caaaed44a0094cd1394a6cfaf86a3ebb456c923
Lenin Sales <leninsalesoliveira@gmail.com>
2025-10-13 18:31:32 -0300

    feat(rotinaExercicio): criar RotinaExercicioRepository estendendo JpaRepository

---
+++ src/main/java/com/leninsales/controle_treino/repositories/RotinaExercicioRepository.java
@@ -0,0 +1,7 @@
+package com.leninsales.controle_treino.repositories;
+
+import com.leninsales.controle_treino.models.RotinaExercicioModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RotinaExercicioRepository extends JpaRepository<RotinaExercicioModel, Integer> {
+}
