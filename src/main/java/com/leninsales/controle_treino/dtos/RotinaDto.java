b45787e907eba4c96331eb11a073d1d2a6f5a6f9
Lenin Sales <leninsalesoliveira@gmail.com>
2025-10-17 16:18:58 -0300

    feat(RotinaDto): Cria DTO de Rotina e validação de entrada

---
+++ src/main/java/com/leninsales/controle_treino/dtos/RotinaDto.java
@@ -0,0 +1,17 @@
+package com.leninsales.controle_treino.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+public record RotinaDto(
+        @NotBlank(message = "Nome da rotina não pode ser nulo.")
+        @Size(min = 3, max = 100, message = "O nome deve ter entre 3 a 100 caracteres.")
+        String nome,
+
+        @NotNull(message = "A lista de exercicio não pode ser nula.")
+        @Size(min = 1, message = "A lista deve conter no minimo um exercicio.")
+        List<RotinaExerciciosDto> exercicios) {
+}
