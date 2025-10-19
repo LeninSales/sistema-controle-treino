45349835c5ffd16ac97c4055cef49ad5b2ee420a
Lenin Sales <leninsalesoliveira@gmail.com>
2025-10-19 17:03:56 -0300

    feat(Rotina): criado dto para contrado de saida junto com método findAllProjectedBy, evita carregamento completo da rotina

---
+++ src/main/java/com/leninsales/controle_treino/dtos/RotinaListagemDto.java
@@ -0,0 +1,12 @@
+package com.leninsales.controle_treino.dtos;
+
+public record RotinaListagemDto(
+
+        String nome,
+
+        Integer idRotina
+
+) {
+
+}
+
--- src/main/java/com/leninsales/controle_treino/repositories/RotinaRepository.java
+++ src/main/java/com/leninsales/controle_treino/repositories/RotinaRepository.java
@@ -1,7 +1,11 @@
 package com.leninsales.controle_treino.repositories;
 
+import com.leninsales.controle_treino.dtos.RotinaListagemDto;
 import com.leninsales.controle_treino.models.RotinaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RotinaRepository extends JpaRepository<RotinaModel, Integer> {
+    List<RotinaListagemDto> findAllProjectedBy();
 }
