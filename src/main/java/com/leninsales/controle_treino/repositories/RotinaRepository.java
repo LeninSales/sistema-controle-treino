eed508f87a862871b488e687721682613a724b39
Lenin Sales <leninsalesoliveira@gmail.com>
2025-10-19 17:19:42 -0300

    refactor(Rotina): adiciona a busca da rotina por id do usuario

--- src/main/java/com/leninsales/controle_treino/repositories/RotinaRepository.java
+++ src/main/java/com/leninsales/controle_treino/repositories/RotinaRepository.java
@@ -5,7 +5,8 @@ import com.leninsales.controle_treino.models.RotinaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface RotinaRepository extends JpaRepository<RotinaModel, Integer> {
-    List<RotinaListagemDto> findAllProjectedBy();
+    List<RotinaListagemDto> findRotinaListagemDtoByIdUsuario(UUID idUsuario);
 }
