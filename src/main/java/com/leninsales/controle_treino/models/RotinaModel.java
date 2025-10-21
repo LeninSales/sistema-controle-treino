4e98fadfcac8c4cf6a0472327fc004db3f644b44
Lenin Sales <leninsalesoliveira@gmail.com>
2025-10-21 16:49:25 -0300

    feat(Rotina): adiciona relação de pai e filho com a entidade RotinaExercicio

--- src/main/java/com/leninsales/controle_treino/models/RotinaModel.java
+++ src/main/java/com/leninsales/controle_treino/models/RotinaModel.java
@@ -1,12 +1,12 @@
 package com.leninsales.controle_treino.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -17,11 +17,19 @@ import java.util.UUID;
 @AllArgsConstructor
 public class RotinaModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idRotina;
 
     private UUID idUsuario;
 
     private String nome;
 
-
+    @OneToMany(
+            cascade = CascadeType.ALL, // <<< ESTE É O SEGREDO
+            fetch = FetchType.LAZY,
+            mappedBy = "rotina",
+            orphanRemoval = true
+    )
+    private List<RotinaExercicioModel> exercicios;
 }
