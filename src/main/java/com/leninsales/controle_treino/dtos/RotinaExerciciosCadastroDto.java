c911b4603469141d1ff3e1fac3b8e895383f2b5f
Lenin Sales <leninsalesoliveira@gmail.com>
2025-10-21 16:46:29 -0300

    feat(RotinaExercicio): adiciona dto pra entrada de dados para o cadastro de rotina

---
+++ src/main/java/com/leninsales/controle_treino/dtos/RotinaExerciciosCadastroDto.java
@@ -0,0 +1,22 @@
+package com.leninsales.controle_treino.dtos;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RotinaExerciciosCadastroDto(
+        @NotNull(message = "Necessário informar o ID do exercício.")
+        Integer idExercicio,
+
+        @NotNull(message = "Necessário informar quantas séries por exercício.")
+        @Min(value = 1, message = "Deve ter no mínimo uma série.")
+        Integer series,
+
+        @NotBlank(message = "As repetições são obrigatórias.")
+        String repeticoes,
+
+        @NotNull(message = "A ordem de execução é obrigatória.")
+        @Min(value = 1, message = "A ordem deve ser um número positivo.")
+        Integer ordem
+) {
+}
