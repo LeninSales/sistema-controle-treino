9993796da55ff87ec67a341806954199a0533194
Lenin Sales <leninsalesoliveira@gmail.com>
2025-10-21 09:35:33 -0300

    feat(RotinaExercicio): adiciona método no repository para buscar rotina atraves do idRotina

--- src/main/java/com/leninsales/controle_treino/repositories/RotinaExercicioRepository.java
+++ src/main/java/com/leninsales/controle_treino/repositories/RotinaExercicioRepository.java
@@ -3,5 +3,9 @@ package com.leninsales.controle_treino.repositories;
 import com.leninsales.controle_treino.models.RotinaExercicioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RotinaExercicioRepository extends JpaRepository<RotinaExercicioModel, Integer> {
+
+    List<RotinaExercicioModel> findByIdRotina(Integer idRotina);
 }
